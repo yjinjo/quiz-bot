0fe882347ce1e0ec1083c473c368d21b6634b129
Youngjin Jo <yjinjo@berkeley.edu>
2022-10-24 14:40:39 +0900

    Run app and Install Pydantic

---
+++ main.py
@@ -0,0 +1,14 @@
+import uvicorn
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get('/')
+def hello():
+    return 'Hello, World!'
+
+
+if __name__ == '__main__':
+    uvicorn.run('main:app', reload=True)
+
