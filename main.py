c0ad0d82388536e149ab0133bdab89ca3fb32d9e
Youngjin Jo <yjinjo@berkeley.edu>
2022-10-24 15:07:27 +0900

    Add type to parameter

--- main.py
+++ main.py
@@ -4,10 +4,9 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get('/')
-def hello():
-    return 'Hello, World!'
-
+@app.get('/users/{user_id}')
+def get_user(user_id: int):
+    return {'user_id': user_id}
 
 if __name__ == '__main__':
     uvicorn.run('main:app', reload=True)
