aa2b42d9ef290f2238296e7f0676b4026b1f3659
Youngjin Jo <yjinjo@berkeley.edu>
2022-10-24 15:49:01 +0900

    Query Parameter and String Enumeration

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 import uvicorn
+
+from enum import Enum
 from fastapi import FastAPI
 
 app = FastAPI()
 
 
-@app.get('/users/{user_id}')
-def get_user(user_id: int):
-    return {'user_id': user_id}
+class UserLevel(str, Enum):
+    a = "a"
+    b = "b"
+    c = "c"
+
+
+@app.get('/users')
+def get_users(grade: UserLevel = UserLevel.a):
+    return {"grade": grade}
+
+
 
 if __name__ == '__main__':
     uvicorn.run('main:app', reload=True)
