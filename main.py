8c57300462f62c10162eeea4858703b9d2e1c0b2
Youngjin Jo <yjinjo@berkeley.edu>
2022-10-24 18:46:53 +0900

    Add Header

--- main.py
+++ main.py
@@ -1,27 +1,11 @@
-from typing import List
-
-from fastapi import FastAPI
-from pydantic import BaseModel, Field
+from fastapi import FastAPI, Header 
 
 app = FastAPI()
 
 
-class Item(BaseModel):
-    name: str = Field(..., min_length=1, max_length=100, title="Name")
-    price: float = Field(None, ge=0)
-    amount: int = Field(
-        defulat = 1,
-        gt = 0,
-        le = 100,
-        title = "Amount",
-        description = "Item's amount. You can have item between 1 and 100"
-    )
-
-
-@app.post("/users/{user_id}/item")
-def create_item(item: Item):
-    return item
-
+@app.get("/header")
+def get_headers(x_token: str = Header(None, title="Token")):
+    return {"X-Token": x_token}
 
 if __name__ == '__main__':
     uvicorn.run('main:app', reload=True)
