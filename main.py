3f46580e8f9160cc7053b6de3e962af298fd9e89
Youngjin Jo <yjinjo@berkeley.edu>
2022-10-30 18:00:31 +0900

    Add Simple Middleware

--- main.py
+++ main.py
@@ -1,20 +1,18 @@
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
+import time
+
+from fastapi import FastAPI, Request
 
 app = FastAPI()
 
-origins = [
-    "http://localhost",
-    "https://localhost:8000",
-]
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["OPTIONS", "GET"],
-    allow_headers=["*"],
-)
+@app.middleware("http")
+async def add_process_time_header(request: Request, call_next):
+    start_time = time.time()
+    response = await call_next(request)
+    process_time = time.time() - start_time
+    response.headers["X-Process-Time"] = str(process_time)
+    return response
+
 
 @app.get("/")
 async def hello():
